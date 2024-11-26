5356ce442fb714a63dec9eac21fc766725c6247b
Venkatesh <kulkarnivenkatesh480@gmail.com>
2024-11-26 17:39:00 +0530

    Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.ssadhan"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.ssadhan"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation ("com.github.denzcoskun:ImageSlideshow:0.1.2")
+    implementation(libs.material)
+    implementation (libs.chaosleung.pinview)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ app/src/main/java/com/example/ssadhan/LanguageActivity.java
@@ -0,0 +1,124 @@
+package com.example.ssadhan;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.example.ssadhan.databinding.ActivityMainBinding;
+
+public class LanguageActivity extends AppCompatActivity {
+    public ActivityMainBinding binding;
+    private int selectedCV = 0;
+    ConstraintLayout layout;
+    TextView textView1,textView2;
+    ImageView rb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.englishCv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (selectedCV == 0){
+                    binding.submitBtn.setBackgroundResource(R.drawable.button_background);
+                    binding.englishLayout.setBackgroundResource(R.drawable.button_selected_background);
+                    binding.englishTv.setTextColor(Color.BLUE);
+                    binding.englishTv2.setTextColor(Color.BLUE);
+                    binding.englishRb.setVisibility(View.VISIBLE);
+                    rb = binding.englishRb;
+                    layout = binding.englishLayout;
+                    textView1 = binding.englishTv;
+                    textView2 = binding.englishTv2;
+                    selectedCV = 1;
+                }
+                else{
+                    layout.setBackgroundResource(R.drawable.cardview_background);
+                    textView1.setTextColor(Color.BLACK);
+                    textView2.setTextColor(Color.BLACK);
+                    rb.setVisibility(View.INVISIBLE);
+                    binding.englishLayout.setBackgroundResource(R.drawable.button_selected_background);
+                    binding.englishTv.setTextColor(Color.BLUE);
+                    binding.englishTv2.setTextColor(Color.BLUE);
+                    binding.englishRb.setVisibility(View.VISIBLE);
+                    layout = binding.englishLayout;
+                    textView1 = binding.englishTv;
+                    textView2 = binding.englishTv2;
+                    rb = binding.englishRb;
+                    selectedCV = 1;
+                }
+            }
+        });
+        binding.marathiCv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (selectedCV == 0){
+                    binding.submitBtn.setBackgroundResource(R.drawable.button_background);
+                    binding.marathiLayout.setBackgroundResource(R.drawable.button_selected_background);
+                    binding.marathiTv.setTextColor(Color.BLUE);
+                    binding.marathiTv2.setTextColor(Color.BLUE);
+                    binding.marathiRb.setVisibility(View.VISIBLE);
+                    rb = binding.marathiRb;
+                    textView1 = binding.marathiTv;
+                    textView2 = binding.marathiTv2;
+                    layout = binding.marathiLayout;
+                    selectedCV = 1;
+                }
+                else{
+                    layout.setBackgroundResource(R.drawable.cardview_background);
+                    textView1.setTextColor(Color.BLACK);
+                    textView2.setTextColor(Color.BLACK);
+                    rb.setVisibility(View.INVISIBLE);
+                    binding.marathiLayout.setBackgroundResource(R.drawable.button_selected_background);
+                    binding.marathiTv.setTextColor(Color.BLUE);
+                    binding.marathiTv2.setTextColor(Color.BLUE);
+                    binding.marathiRb.setVisibility(View.VISIBLE);
+                    layout = binding.marathiLayout;
+                    textView1 = binding.marathiTv;
+                    textView2 = binding.marathiTv2;
+                    rb = binding.marathiRb;
+                    selectedCV = 1;
+                }
+            }
+        });
+        binding.hindiCv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (selectedCV == 0){
+                    binding.submitBtn.setBackgroundResource(R.drawable.button_background);
+                    binding.hindiLayout.setBackgroundResource(R.drawable.button_selected_background);
+                    binding.hindiTv.setTextColor(Color.BLUE);
+                    binding.hindiTv2.setTextColor(Color.BLUE);
+                    binding.hindiRb.setVisibility(View.VISIBLE);
+                    rb = binding.hindiRb;
+                    textView1 = binding.hindiTv;
+                    textView2 = binding.hindiTv2;
+                    layout = binding.hindiLayout;
+                    selectedCV = 1;
+                }
+                else{
+                    layout.setBackgroundResource(R.drawable.cardview_background);
+                    textView1.setTextColor(Color.BLACK);
+                    textView2.setTextColor(Color.BLACK);
+                    rb.setVisibility(View.INVISIBLE);
+                    binding.hindiLayout.setBackgroundResource(R.drawable.button_selected_background);
+                    binding.hindiTv.setTextColor(Color.BLUE);
+                    binding.hindiTv2.setTextColor(Color.BLUE);
+                    binding.hindiRb.setVisibility(View.VISIBLE);
+                    layout = binding.hindiLayout;
+                    textView1 = binding.hindiTv;
+                    textView2 = binding.hindiTv2;
+                    rb = binding.hindiRb;
+                    selectedCV = 1;
+                }
+            }
+        });
+
+    }
+}
